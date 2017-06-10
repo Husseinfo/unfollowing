77b9eeb1df1b7724d1e9947aa528b34d8268cf60
Hussein <husseinfo@mail.com>
2017-06-10 14:12:51 +0300

    just unfollow 🔥

--- unfollow.js
+++ unfollow.js
@@ -1,15 +1,17 @@
 function checkUnfollowers() {
     var users = $('.ProfileCard-content');
+
     $(users).each(function() {
-        followStatus = $(this).find('.FollowStatus');
-        if(!$(followStatus).text()) {
+        following = $(this).find('.FollowStatus').text();
+
+        if(!following) {
             name = $(this).find('.fullname.ProfileNameTruncated-link.u-textInheritColor.js-nav').text();
             username = $(this).find('.u-linkComplex-target').text();
             unfollowButton = $(this).find('.EdgeButton.EdgeButton--danger.EdgeButton--small.button-text.unfollow-text');
-    	if (confirm(name + ' (@' + username + ')\n is not following you, unfollow?')) {
+
+            if (confirm(name + ' (@' + username + ')\n is not following you, unfollow?')) {
                 $(unfollowButton).click();
             }
         }
     });
 }
-
